2cae4fa56ac109876c4b7bbe851d073bb7e98a1c
relshenawy <r.elshenawy@gmail.com>
2011-11-30 12:23:12 -0500

    db  fix

--- db/migrate/20111126183108_add_functional_to_roles.rb
+++
@@ -1,9 +0,0 @@
-class AddFunctionalToRoles < ActiveRecord::Migration
-  def self.up
-    add_column :roles, :functional, :boolean
-  end
-
-  def self.down
-    remove_column :roles, :functional
-  end
-end
--- db/migrate/20111127161633_remove_course_id_from_project_classes.rb
+++
@@ -1,9 +0,0 @@
-class RemoveCourseIdFromProjectClasses < ActiveRecord::Migration
-  def self.up
-    remove_column :project_classes, :course_id
-  end
-
-  def self.down
-    add_column :project_classes, :course_id, :integer
-  end
-end
